4eed92f7f8f7fbde0b10db2c0544158bfb9f785d
GeneralMud <43315269+GeneralMud@users.noreply.github.com>
2018-09-16 17:56:10 +0300

    remove regex and simplify the url routing

--- data/backend/socialrest/api/api/urls.py
+++ data/backend/socialrest/api/api/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^api/blogs', include('rest.urls')),
+    path('api/blogs/', include('rest.urls')),
 ]
